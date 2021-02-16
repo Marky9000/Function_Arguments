b2a81609cc57d18cae03ef5b44b49ad3b88af5ea
Mark van Booren <markvanbooren@hotmail>
2021-02-16 17:14:40 +0100

    substract2

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const paintingTheWall = function(wall, color){
+     console.log("the " + wall + " wall is painted " + color);
+}
+
+
+paintingTheWall("north", "green");
+
+// // const heat = function(item){
+// //     console.log("I'm heating " + item);
+// //     console.log("Ping! your "+item+ " is ready!")
+
+// // }
+
+// // const main_course = "Pasta";
+// // const dessert = "ice";
+
+
+// // heat(main_course);
+// // heat(dessert);
+
+// // const multiply = function ( num1, num2, num3){
+// //     console.log(num1 * num2 * num3)
+// // }
+
+// // multiply(2434, 3535, 3636 );
+
+
+// const calculateAverage = function(numbers) {
+//     //calc average
+//     const average = numbers.reduce((total, n) => total+n)/ numbers.length;
+//     console.log(average);
+// };
+
+// calculateAverage([2, 4]);
+
+// const grades = [6,7,8,9,5,4,6,5,6];
+// calculateAverage(grades);
+// const ages = [12,45,79,6];
+// calculateAverage(ages);
+            
